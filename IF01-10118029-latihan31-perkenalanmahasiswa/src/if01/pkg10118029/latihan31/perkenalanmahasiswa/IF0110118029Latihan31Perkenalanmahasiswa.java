355e6bc2a40596d04341335f6b1e6126197f5ac6
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-10-17 16:28:56 +0700

    Add files via upload

---
+++ IF01-10118029-latihan31-perkenalanmahasiswa/src/if01/pkg10118029/latihan31/perkenalanmahasiswa/IF0110118029Latihan31Perkenalanmahasiswa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118029.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author acer
+ * Nama  : Azis komara
+ * Nim   : 10118029
+ * Kelas : IF-1 
+ * Deskripsi program : Program ini menampilkan PERKENALAN MAHASISWA
+ * 
+ */
+public class IF0110118029Latihan31Perkenalanmahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         mahasiswa mahasiswa = new mahasiswa();
+           
+            
+            
+            mahasiswa. nim = "10118029";
+            mahasiswa.nama ="Azis komara";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118028";
+            mahasiswa.nama = "Surya";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118012";
+            mahasiswa.nama = "moc rizki maulana";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118008";
+            mahasiswa.nama = "mimel";
+            mahasiswa.perkenalanDiri();
+    }
+    
+}
---
+++ IF01-10118029-latihan31-perkenalanmahasiswa/src/if01/pkg10118029/latihan31/perkenalanmahasiswa/mahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118029.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author acer
+ * Nama  : Azis komara
+ * Nim   : 10118029
+ * Kelas : IF-1 
+ * Deskripsi program : Program ini menampilkan PERKENALAN MAHASISWA
+ * 
+ */
+public class mahasiswa {
+    
+    public String nim;
+    public String nama;
+
+	public void perkenalanDiri() {
+	      
+            System.out.println("Hallo Everyone");
+            System.out.println("My NIM is " + nim);
+            System.out.println("My name is " + nama);
+            System.out.println();
+	}
+    
+}
